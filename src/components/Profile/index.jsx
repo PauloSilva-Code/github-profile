34116dcb59f9a70cf5c9fcbe86db982403f183e1
Paulo Silva <paulowrsilva@gmail.com>
2024-07-16 11:09:33 -0300

    style: add styled component no Profile

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,25 +3,33 @@ import * as S from "./styled";
 const Profile = () => {
   return (
     <S.Wrapper>
-      <img
+      <S.WrapperImage
         src="https://avatars.githubusercontent.com/u/133034863?v=4"
         alt="Avatar of user"
       />
-      <h1>Paulo Silva</h1>
-      <h3>Username: </h3>
-      <span>Paulo</span>
-      <div>
-        <h4>Followers</h4>
-        <span>5</span>
-      </div>
-      <div>
-        <h4>Starred</h4>
-        <span>5</span>
-      </div>
-      <div>
-        <h4>Followings</h4>
-        <span>5</span>
-      </div>
+      <S.WrapperInfoUser>
+        <div>
+          <h1>Paulo Silva</h1>
+          <S.WrapperUsername>
+            <h3>Username: </h3>
+            <span>Paulo</span>
+          </S.WrapperUsername>
+        </div>
+        <S.WrapperStatusCount>
+          <div>
+            <h4>Followers</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starred</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>5</span>
+          </div>
+        </S.WrapperStatusCount>
+      </S.WrapperInfoUser>
     </S.Wrapper>
   );
 };
--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -2,8 +2,31 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  /* align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const WrapperInfoUser = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
   flex-direction: column;
-  height: 100vh; */
+  height: auto;
+  margin-left: 8px;
+`;
+
+export const WrapperStatusCount = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const WrapperUsername = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const WrapperImage = styled.img`
+  border-radius: 50%;
+  width: 200px;
+  margin: 8px;
 `;
